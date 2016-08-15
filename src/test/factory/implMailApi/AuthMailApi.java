fe390df9263e02f8ebd4c5b1e11caf49f1cf3a29
kriakaziaba <kriakaziaba@gmail.com>
2016-08-15 12:39:54 +0300

    add abstract factory

---
+++ src/test/factory/implMailApi/AuthMailApi.java
@@ -0,0 +1,9 @@
+package test.factory.implMailApi;
+
+import test.factory.interfaces.Auth;
+
+/**
+ * Created by tku on 8/15/2016.
+ */
+public class AuthMailApi implements Auth {
+}
---
+++ src/test/factory/implWebDriver/AuthWebDriver.java
@@ -0,0 +1,9 @@
+package test.factory.implWebDriver;
+
+import test.factory.interfaces.Auth;
+
+/**
+ * Created by tku on 8/15/2016.
+ */
+public class AuthWebDriver implements Auth {
+}
