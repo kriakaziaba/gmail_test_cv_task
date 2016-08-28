f64bd2efa1b26fea8efe1176998085c1043a018b
kriakaziaba <kriakaziaba@gmail.com>
2016-08-29 01:42:03 +0300

    migrate to gradle

--- src/test/tests/TestCases1.java
+++ src/test/tests/TestCases1.java
@@ -11,7 +11,12 @@ public class TestCases1 extends BaseTest {
 
     @BeforeMethod
     public void cleanUp(){
-        app.preconditions();
+        try {
+            app.preconditions();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
     @Test(dataProvider = "forAuth")
